539709b19e6bd54cc2becc95f3ba7747ff2d7149
Andrew McCartney <acm9q@virginia.edu>
2019-10-09 11:24:56 -0400

    is now a function.

--- display.R
+++ display.R
@@ -20,41 +20,83 @@ filter(Candidate != "deBlasio")  %>%
 filter(Candidate != "Steyer")
 
 
-palate<-c(Warren = "#115740", Bennet = "#b9975b",
-          Booker = "#00b388", Bullock = "#cab64b", Buttigieg = "#64ccc9", Castro = "#789D4a",
-          Gabbard= "#789f90", ORourke =  "#5b6770", Klobuchar = "#f0b323",
-          Harris = "#83434e", Sanders = "#e56a54", Biden = "#183028", Williamson = "#00313c",
-          Yang = "#cc5500")
-dat2  %>% 
-# filter(Candidate %in% c("Warren", "Biden", "Harris", "Sanders"))  %>% 
-    ggplot(aes( 
-        x = mdy(.$date), y = Percent, color = Candidate
-    )) + 
-    geom_smooth(
-        aes(linetype = Candidate), 
-        method = "loess", 
-        span = .27
-    ) + 
-   # geom_point(alpha = 0.2) +
-   # geom_jitter(alpha = 0.2) +
-    theme_light() + 
-    # scale_colour_wm() + 
-   scale_color_manual(values = palate) +
-    labs(
-    title = "Average of Polls with error", 
-        x = "Date") +
-    # phaselines for debates:
-    geom_vline(xintercept = as.numeric(as.Date("2019-06-27")), alpha = 0.3) + 
-    geom_vline(xintercept = as.numeric(as.Date("2019-07-31")), alpha = 0.3) + 
-    geom_vline(xintercept = as.numeric(as.Date("2019-09-12")), alpha = 0.3) + 
-    # geom_hline(yintercept = 26.5) + #Biden's lowest point so far?
-    scale_y_continuous(limits = c(0,40)) + 
-    scale_x_date(
-        #limits= c(as.Date("2019-06-1"), as.Date("2019-09-17")), #arbitrary date censoring
-        breaks = date_breaks("months"),
-      labels = date_format("%b")) + 
-    NULL
+run_graphic<-function(dat2, points = FALSE, restricted = FALSE){
+    palate<-c(Warren = "#115740", Bennet = "#b9975b", Booker = "#00b388", Bullock = "#cab64b", 
+              Buttigieg = "#64ccc9", Castro = "#789D4a",Gabbard= "#789f90", ORourke =  "#5b6770", 
+              Klobuchar = "#f0b323",Harris = "#83434e", Sanders = "#e56a54", Biden = "#183028", 
+              Williamson = "#00313c",Yang = "#cc5500")
+
+
+    p <- if(restricted == TRUE){
+        dat2  %>% 
+        filter(Candidate %in% c("Warren",  "Biden", "Sanders"))  %>% 
+        ggplot(aes( 
+            x = mdy(.$date), y = Percent, color = Candidate
+        )) + 
+        geom_smooth(
+            aes(linetype = Candidate), 
+            method = "loess", 
+           # method = "lm"
+            span = .27
+        ) + 
+       #geom_point(alpha = 0.2, stroke = 0) +
+        #geom_jitter(alpha = 0.2) +
+        theme_light() + 
+       scale_color_manual(values = palate) +
+        labs(
+            title = "Average of Polls with error", 
+            subtitle = today(),
+            x = "Date") +
+        geom_vline(xintercept = as.numeric(as.Date("2019-06-27")), alpha = 0.3) + 
+        geom_vline(xintercept = as.numeric(as.Date("2019-07-31")), alpha = 0.3) + 
+        geom_vline(xintercept = as.numeric(as.Date("2019-09-12")), alpha = 0.3) + 
+        geom_vline(xintercept = as.numeric(as.Date("2019-10-15")), alpha = 0.3) +   
+        #geom_hline(yintercept = 26.7) +
+        scale_y_continuous(limits = c(0,40), 
+                          breaks = count_to_by(40,5) )+ 
+        scale_x_date(
+            #limits= c(as.Date("2019-03-1"), today()),
+            breaks = date_breaks("months"),
+          labels = date_format("%b")) + 
+        NULL
+        } else {
+        dat2  %>% 
+        #filter(Candidate %in% c("Warren",  "Biden"))  %>% 
+        ggplot(aes( 
+            x = mdy(.$date), y = Percent, color = Candidate
+        )) + 
+        geom_smooth(
+            aes(linetype = Candidate), 
+            method = "loess", 
+           # method = "lm"
+            span = .27
+        ) + 
+       #geom_point(alpha = 0.2, stroke = 0) +
+        #geom_jitter(alpha = 0.2) +
+        theme_light() + 
+       scale_color_manual(values = palate) +
+        labs(
+            title = "Average of Polls with error", 
+            subtitle = today(),
+            x = "Date") +
+        geom_vline(xintercept = as.numeric(as.Date("2019-06-27")), alpha = 0.3) + 
+        geom_vline(xintercept = as.numeric(as.Date("2019-07-31")), alpha = 0.3) + 
+        geom_vline(xintercept = as.numeric(as.Date("2019-09-12")), alpha = 0.3) + 
+        geom_vline(xintercept = as.numeric(as.Date("2019-10-15")), alpha = 0.3) +   
+        #geom_hline(yintercept = 26.7) +
+        scale_y_continuous(limits = c(0,40), 
+                          breaks = count_to_by(40,5) )+ 
+        scale_x_date(
+            #limits= c(as.Date("2019-03-1"), today()),
+            breaks = date_breaks("months"),
+          labels = date_format("%b")) + 
+        NULL
+    }
     
+    if (points == TRUE){
+        p + geom_point(alpha = 0.2, stroke = 0) 
+    } else {return(p)}
+}
     
     
  # facet:
